8431aa5610b13633b77fd376da91f3fc5de50c90
SH22Hwang <70620484+SH22Hwang@users.noreply.github.com>
2021-06-13 17:30:53 +0900

    init

---
+++ li_client_clone.py
@@ -0,0 +1,41 @@
+import cv2
+import socket
+import time
+from threading import Thread
+
+HOST = 'localhost'
+PORT = 9999
+
+
+class ScreenShare():
+    pass;
+
+def rcvMsg(sock):
+    while True:
+        try:
+            data = sock.recv(1024)
+            if not data:
+                break
+            print(data.decode())
+        except:
+            pass
+
+
+def runChat():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        sock.connect((HOST, PORT))
+        t = Thread(target=rcvMsg, args=(sock,))
+        t.daemon = True
+        t.start()
+
+        while True:
+            time.sleep(0.5)
+            msg = input(">>>")
+            if msg == '/quit':
+                sock.send(msg.encode())
+                break
+
+            sock.send(msg.encode())
+
+
+runChat()
